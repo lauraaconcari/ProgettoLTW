854a7e4ae06fd550a66f932ef407da1bf7a349ae
MarcelloCap <capobianco.1942389@studenti.uniroma1.it>
2023-04-14 18:27:26 +0200

    fix più municipio secondo centrato

--- Municipi/Municipio II/script.js
+++ Municipi/Municipio II/script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([41.90187547065311, 12.473100381372474], 13);
+var map = L.map('map').setView([41.9227557539587, 12.498165206192587], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
