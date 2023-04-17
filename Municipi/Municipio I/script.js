227e9205e060bdc7f2d3a8bb3c3593cc5d7e24d7
MarcelloCap <capobianco.1942389@studenti.uniroma1.it>
2023-04-17 16:32:36 +0200

    Municipio Primo riempito

--- Municipi/Municipio I/script.js
+++ Municipi/Municipio I/script.js
@@ -24,23 +24,64 @@ var Rione17 = L.marker([41.891619047700715, 12.470187910150768]).bindPopup("Bar 
 var Rione18 = L.marker([41.89222505646582, 12.467839254077079]).bindPopup("Polpetta Trastevere").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
 var Rione19 = L.marker([41.88989831282052, 12.473302665118123]).bindPopup("VinAllegro").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
 var Rione20 = L.marker([41.891403950975615, 12.470768885257653]).bindPopup("Freni e Frizioni").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione21 = L.marker([41.903802099573944, 12.462539854077702]).bindPopup("Il Petruccino").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione22 = L.marker([41.904677356699445, 12.460428782912816]).bindPopup("Makasar Bistròt").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione23 = L.marker([41.89399424386522, 12.50387915407721]).bindPopup("Gatsby Cafè").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione24 = L.marker([41.9008678971654, 12.498200025242427]).bindPopup("Apotheke Cocktail Bar").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione25 = L.marker([41.88741291239995, 12.5104307099014]).bindPopup("Hoppiness").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione26 = L.marker([41.90798242500299, 12.49654281543946]).bindPopup("Downing Square Tartare Bar").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione27 = L.marker([41.904365187378616, 12.495215742428718]).bindPopup("Lumen Cocktails & Cuisine").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione28 = L.marker([41.907967346786094, 12.502070713288644]).bindPopup("La Caramella").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione29 = L.marker([41.890103534412006, 12.494992065151086]).bindPopup("Coming Out").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione30 = L.marker([41.88984477434638, 12.495400427087581]).bindPopup("Colosseum Bar").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione31 = L.marker([41.88908603670206, 12.497266196406766]).bindPopup("Giulia&Sisto Roma").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione32 = L.marker([41.89028496034733, 12.495634598252483]).bindPopup("The Court").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione33 = L.marker([41.87868267183433, 12.479464181065802]).bindPopup("Tram Depot Testaccio").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione34 = L.marker([41.87985171097752, 12.476583691575266]).bindPopup("Il Testaccino").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione35 = L.marker([41.88201004049032, 12.476610009901071]).bindPopup("Il Gianfornaio - Testaccio").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione36 = L.marker([41.88306872570846, 12.486731352230947]).bindPopup("Rosso Eat Drink Stay").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione37 = L.marker([41.8811018306263, 12.485096709901011]).bindPopup("Casa Manfredi").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione38 = L.marker([41.89896006272525, 12.472407755280765]).bindPopup("Terrazza Borromini").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Rione39 = L.marker([41.89674832144241, 12.490165529769195]).bindPopup("Rooftop Spritzeria Monti").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
 
 
-
-var Rioni = L.layerGroup([Rione1, Rione2, Rione3, Rione4, Rione5, Rione6, Rione7, Rione8, Rione9, Rione10, Rione11, Rione12, Rione13, Rione14, Rione15, Rione16, Rione17, Rione18, Rione19, Rione20]);
+var Rioni = L.layerGroup([Rione1, Rione2, Rione3, Rione4, Rione5, Rione6, Rione7, Rione8, Rione9, Rione10,
+Rione11, Rione12, Rione13, Rione14, Rione15, Rione16, Rione17, Rione18, Rione19, Rione20,Rione21, Rione22,
+Rione23, Rione24, Rione25, Rione26, Rione27, Rione28, Rione29, Rione30, Rione31, Rione32, Rione33, Rione34,
+Rione35, Rione36, Rione37, Rione38, Rione39]);
 Rioni.addTo(map);
 
 //Prati:
 var Prati1 = L.marker([41.90884691477165, 12.467018598253418]).bindPopup("Vino e Focaccia").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
 var Prati2 = L.marker([41.909184420221294, 12.448137184656451]).bindPopup("Fischio").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
 var Prati3 = L.marker([41.90667086018015, 12.45659242524276]).bindPopup("200 Gradi").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Prati = L.layerGroup([Prati1, Prati2, Prati3]);
+var Prati4 = L.marker([41.90690311578641, 12.472552393490021]).bindPopup("Freeda Roma").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prati5 = L.marker([41.91261690392565, 12.468776396407948]).bindPopup("Dafne Garden Cafè").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prati6 = L.marker([41.90866304941401, 12.465132807491386]).bindPopup("Il Gianfornaio - Prati").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prati7 = L.marker([41.90683191774508, 12.476212157016242]).bindPopup("Il Maritozzo Rosso - Prati").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prati8 = L.marker([41.90965782505645, 12.473913630535083]).bindPopup("Vinoteca Tempere Prati").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prati9 = L.marker([41.907370764930114, 12.46653112524273]).bindPopup("Ozio Restaurant").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+
+var Prati = L.layerGroup([Prati1, Prati2, Prati3,Prati4, Prati5, Prati6, Prati7, Prati8, Prati9]);
 Prati.addTo(map);
 //Della Vittoria
+41.917122983756435, 12.460447019143468
+var Vittoria1 = L.marker([41.91708314063197, 12.46051141174866]).bindPopup("Quintessenza").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Vittoria2 = L.marker([41.92349370726969, 12.459005273110476]).bindPopup("Prato Chiosco").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Vittoria3 = L.marker([41.920519716796804, 12.460604725243531]).bindPopup("Etilico").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+
+var Vittoria = L.layerGroup([Vittoria1,Vittoria2,Vittoria3]);
+Vittoria.addTo(map);
 //Eroi
 var Eroi1=L.marker([41.90953640338526, 12.446431820849735]).bindPopup("Mama Shelter Roma").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Eroi2=L.marker([41.90892453707703, 12.452025865726986]).bindPopup("Cantinero").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Eroi3=L.marker([41.90977338379914, 12.448700396407805]).bindPopup("Bolle Cicchetteria di Quartiere").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Eroi4=L.marker([41.909536128362696, 12.449852396407822]).bindPopup("TBeB").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Eroi5=L.marker([41.91530812657665, 12.462366382913403]).bindPopup("Mazzini Caffetteria - Fergui S.r.l.s.").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Eroi6=L.marker([41.91553532817841, 12.464428261461045]).bindPopup("Gran Caffè Mazzini").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Eroi7=L.marker([41.91658982540807, 12.461012000099565]).bindPopup("Tonico Café").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
 
-var Eroi=L.layerGroup([Eroi1]);
+var Eroi=L.layerGroup([Eroi1,Eroi2,Eroi3,Eroi4,Eroi5,Eroi6,Eroi7]);
 Eroi.addTo(map);
 
 
