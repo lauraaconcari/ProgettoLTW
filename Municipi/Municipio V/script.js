f3cb9fd9eb523fe182235b81ffd1ce8572d89bfc
MarcelloCap <capobianco.1942389@studenti.uniroma1.it>
2023-04-19 17:39:24 +0200

    Laura e i sync prematuri

--- Municipi/Municipio V/script.js
+++ Municipi/Municipio V/script.js
@@ -4,15 +4,25 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-//Torpignattara
-//Casilino
-//Quadraro
-//Gordiani
-//Centocelle
-//Alessandrina
-//Tor Sapienza
-//La Rustica
-//Tor Tre Teste
-//Casetta Mistica
-//Centro Direzionale Centocelle
-//Omo
+//Prenestino_Labicano
+var Prenestino_Labicano1 = L.marker([41.88997970881795, 12.523782085523587]).bindPopup("Bar Sciubba Roma").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano2 = L.marker([41.8897319746865, 12.523932651254176]).bindPopup("Pigneto Cafe' & Spirits").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano3 = L.marker([41.8893975489319, 12.525010985441915]).bindPopup("Pigneto Quarantuno").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano4 = L.marker([41.88906218657238, 12.525414202859203]).bindPopup("Pigneto 32").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano5 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano6 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano7 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano8 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano9 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano10 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano11 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano12 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano13 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano14 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano15 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano16 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano17 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano18 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano19 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano19 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
+var Prenestino_Labicano20 = L.marker([]).bindPopup("").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
