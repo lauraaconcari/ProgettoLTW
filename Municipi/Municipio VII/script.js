c9261e9dc632de7adef5ea62b2389b5aff031666
lauraaconcari <concari.1890490@studenti.uniroma1.it>
2023-04-21 18:56:06 +0200

    solo excel manco finito ma quanto cazzo è grande il 7 municipio

--- Municipi/Municipio VII/script.js
+++ Municipi/Municipio VII/script.js
@@ -3,13 +3,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var customIcon = L.icon({
+    iconUrl:'../../placeholder.png' ,
+    iconSize: [17, 17] 
+  });
+  L.Marker.prototype.options.icon = customIcon;
 
-//Torrespaccata
-//Tuscolano Nord
-//Tuscolano Sud
-//Tor Fiscale
-//Appio
-//Latino
+//Tuscolano 
+
 //Don Bosco
 //Appio Claudio
 //Borghesiana 
