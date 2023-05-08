49060b3f569bc08b57ea4b19a7d4f255ba0dfd88
lauraaconcari <concari.1890490@studenti.uniroma1.it>
2023-05-09 00:05:13 +0200

    faccio sempre commit prima di centrare bene

--- Municipi/Municipio XIII/script.js
+++ Municipi/Municipio XIII/script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([41.89948392899482, 12.399890452712516], 13);
+var map = L.map('map').setView([41.89592576336687, 12.433252152089192], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
