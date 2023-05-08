ccbed6f116435ad0618538b432eaa51adaf3c76c
lauraaconcari <concari.1890490@studenti.uniroma1.it>
2023-05-08 22:07:21 +0200

    centrata meglio la mappa

--- Municipi/Municipio XII/script.js
+++ Municipi/Municipio XII/script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([41.86990113399812, 12.431758843140505], 13);
+var map = L.map('map').setView([41.87686627372305, 12.451200570483474], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
