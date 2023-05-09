d29e465c759c0b55fda754e0965901033f8df7b5
MarcelloCap <capobianco.1942389@studenti.uniroma1.it>
2023-05-09 12:46:19 +0200

    Cluster aggiunti ai municipi

--- Municipi/Municipio VII/script.js
+++ Municipi/Municipio VII/script.js
@@ -10,84 +10,89 @@ var customIcon = L.icon({
   L.Marker.prototype.options.icon = customIcon;
 
 //Tuscolano 
-var Tuscolano1 = L.marker([41.8839415852973, 12.522563025486251]).bindPopup("Tira e molla").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano2 = L.marker([41.88375802271953, 12.518130338978064]).bindPopup("Bruni Bistrot Cocktail Bar").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano3 = L.marker([41.88248037583988, 12.51540458315826]).bindPopup("Salotto Stadlin").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano4 = L.marker([41.88584730515517, 12.51174661199444]).bindPopup("BAR BRINDISI").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano5 = L.marker([41.88556992000116, 12.50915609665027]).bindPopup("Le Foodie Café Bistrot").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano6 = L.marker([41.88690186502841, 12.5191475813061]).bindPopup("Blind Pig").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano7 = L.marker([41.88482305866436, 12.51367121199441]).bindPopup("VERSO Eatery & Wine Tales").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano8 = L.marker([41.88506879358799, 12.512587388741487]).bindPopup("C'era una volta il Caffé").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano9 = L.marker([41.884045673598614, 12.51116995432233]).bindPopup("Piano C - Circolo del vino").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano10 = L.marker([41.881417741777256, 12.515212311994208]).bindPopup("Sapori e Parole").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano11 = L.marker([41.873020406676154, 12.528975396649617]).bindPopup("FloW").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano12 = L.marker([41.87606120894121, 12.52985229664983]).bindPopup("Spigolo").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano13 = L.marker([41.87204944028381, 12.525550596649545]).bindPopup("Babù").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano14 = L.marker([41.8627859543976, 12.546941791067944]).bindPopup("malti&mosti").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano16 = L.marker([41.877142755713415, 12.52051921992895]).bindPopup("NaBi Happiness Factory").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano17 = L.marker([41.8768250446149, 12.520886383157906]).bindPopup("TEO'S - Piazza dell'Alberone").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano18 = L.marker([41.87736656295493, 12.518967783158018]).bindPopup("Kubla/Khan").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano19 = L.marker([41.87082069916019, 12.5305424082891]).bindPopup("Quality to Drink").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano20 = L.marker([41.87381919317392, 12.529234296649674]).bindPopup("Les Amis").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano21 = L.marker([41.86465207162865, 12.54213106966543]).bindPopup("Caffedotto Roma").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano22 = L.marker([41.86329362401135, 12.547890110140973]).bindPopup("SECONDO GIRO").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano23 = L.marker([41.86289814678021, 12.550876740829278]).bindPopup("Officina del Sapore").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano24 = L.marker([41.86374289218648, 12.552964538977065]).bindPopup("Barley Wine").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano25 = L.marker([41.85963901217739, 12.551802627337239]).bindPopup("Sottosopra").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Tuscolano26 = L.marker([41.86133660857102, 12.548430356173373]).bindPopup("spillo").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-
+var Tuscolano1 = L.marker([41.8839415852973, 12.522563025486251]).bindPopup("Tira e molla");
+var Tuscolano2 = L.marker([41.88375802271953, 12.518130338978064]).bindPopup("Bruni Bistrot Cocktail Bar");
+var Tuscolano3 = L.marker([41.88248037583988, 12.51540458315826]).bindPopup("Salotto Stadlin");
+var Tuscolano4 = L.marker([41.88584730515517, 12.51174661199444]).bindPopup("BAR BRINDISI");
+var Tuscolano5 = L.marker([41.88556992000116, 12.50915609665027]).bindPopup("Le Foodie Café Bistrot");
+var Tuscolano6 = L.marker([41.88690186502841, 12.5191475813061]).bindPopup("Blind Pig");
+var Tuscolano7 = L.marker([41.88482305866436, 12.51367121199441]).bindPopup("VERSO Eatery & Wine Tales");
+var Tuscolano8 = L.marker([41.88506879358799, 12.512587388741487]).bindPopup("C'era una volta il Caffé");
+var Tuscolano9 = L.marker([41.884045673598614, 12.51116995432233]).bindPopup("Piano C - Circolo del vino");
+var Tuscolano10 = L.marker([41.881417741777256, 12.515212311994208]).bindPopup("Sapori e Parole");
+var Tuscolano11 = L.marker([41.873020406676154, 12.528975396649617]).bindPopup("FloW");
+var Tuscolano12 = L.marker([41.87606120894121, 12.52985229664983]).bindPopup("Spigolo");
+var Tuscolano13 = L.marker([41.87204944028381, 12.525550596649545]).bindPopup("Babù");
+var Tuscolano14 = L.marker([41.8627859543976, 12.546941791067944]).bindPopup("malti&mosti");
+var Tuscolano16 = L.marker([41.877142755713415, 12.52051921992895]).bindPopup("NaBi Happiness Factory");
+var Tuscolano17 = L.marker([41.8768250446149, 12.520886383157906]).bindPopup("TEO'S - Piazza dell'Alberone");
+var Tuscolano18 = L.marker([41.87736656295493, 12.518967783158018]).bindPopup("Kubla/Khan");
+var Tuscolano19 = L.marker([41.87082069916019, 12.5305424082891]).bindPopup("Quality to Drink");
+var Tuscolano20 = L.marker([41.87381919317392, 12.529234296649674]).bindPopup("Les Amis");
+var Tuscolano21 = L.marker([41.86465207162865, 12.54213106966543]).bindPopup("Caffedotto Roma");
+var Tuscolano22 = L.marker([41.86329362401135, 12.547890110140973]).bindPopup("SECONDO GIRO");
+var Tuscolano23 = L.marker([41.86289814678021, 12.550876740829278]).bindPopup("Officina del Sapore");
+var Tuscolano24 = L.marker([41.86374289218648, 12.552964538977065]).bindPopup("Barley Wine");
+var Tuscolano25 = L.marker([41.85963901217739, 12.551802627337239]).bindPopup("Sottosopra");
+var Tuscolano26 = L.marker([41.86133660857102, 12.548430356173373]).bindPopup("spillo");
 var Tuscolano = L.layerGroup([Tuscolano1, Tuscolano2, Tuscolano3, Tuscolano4, Tuscolano5, Tuscolano6, Tuscolano7, Tuscolano8, 
     Tuscolano9, Tuscolano10, Tuscolano11, Tuscolano12, Tuscolano13, Tuscolano14, Tuscolano16, Tuscolano17, Tuscolano18, 
     Tuscolano19, Tuscolano20, Tuscolano21, Tuscolano22, Tuscolano23, Tuscolano24, Tuscolano25, Tuscolano26]);
-Tuscolano.addTo(map);
+
 
 
 
 //Don Bosco
-var DonBosco1 = L.marker([41.8604769820637, 12.557884754321007]).bindPopup("Al Solito Posto").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var DonBosco2 = L.marker([41.8592847850433, 12.566642611993059]).bindPopup("Rquadro").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var DonBosco3 = L.marker([41.85578385837417, 12.56501098315682]).bindPopup("Sicilian's gourmet").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-
+var DonBosco1 = L.marker([41.8604769820637, 12.557884754321007]).bindPopup("Al Solito Posto");
+var DonBosco2 = L.marker([41.8592847850433, 12.566642611993059]).bindPopup("Rquadro");
+var DonBosco3 = L.marker([41.85578385837417, 12.56501098315682]).bindPopup("Sicilian's gourmet");
 var DonBosco = L.layerGroup([DonBosco1, DonBosco2, DonBosco3]);
 DonBosco.addTo(map);
 
 //Appio Claudio
-var AppioClaudio1 = L.marker([41.85413465849214, 12.559368825484686]).bindPopup("La dolce vita").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioClaudio2 = L.marker([41.85420291506349, 12.56334335432074]).bindPopup("Cocktail Bar 23 & Food").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioClaudio3 = L.marker([41.8546949790746, 12.564139194796343]).bindPopup("La Bonora").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioClaudio4 = L.marker([41.85445263188845, 12.566336423632457]).bindPopup("Démodé").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioClaudio5 = L.marker([41.85295383488472, 12.567254923632301]).bindPopup("Rab").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioClaudio6 = L.marker([41.85786049771943, 12.56108859664883]).bindPopup("SpaccioVino Tuscolana").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioClaudio7 = L.marker([41.85841809357932, 12.558788997265356]).bindPopup("Happy Wine").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioClaudio8 = L.marker([41.85440339724227, 12.565572325484691]).bindPopup("Maat Bakery & Bistrot").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioClaudio9 = L.marker([41.85197951168113, 12.55924931199269]).bindPopup("Fermentum").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-
+var AppioClaudio1 = L.marker([41.85413465849214, 12.559368825484686]).bindPopup("La dolce vita");
+var AppioClaudio2 = L.marker([41.85420291506349, 12.56334335432074]).bindPopup("Cocktail Bar 23 & Food");
+var AppioClaudio3 = L.marker([41.8546949790746, 12.564139194796343]).bindPopup("La Bonora");
+var AppioClaudio4 = L.marker([41.85445263188845, 12.566336423632457]).bindPopup("Démodé");
+var AppioClaudio5 = L.marker([41.85295383488472, 12.567254923632301]).bindPopup("Rab");
+var AppioClaudio6 = L.marker([41.85786049771943, 12.56108859664883]).bindPopup("SpaccioVino Tuscolana");
+var AppioClaudio7 = L.marker([41.85841809357932, 12.558788997265356]).bindPopup("Happy Wine");
+var AppioClaudio8 = L.marker([41.85440339724227, 12.565572325484691]).bindPopup("Maat Bakery & Bistrot");
+var AppioClaudio9 = L.marker([41.85197951168113, 12.55924931199269]).bindPopup("Fermentum");
 var AppioClaudio = L.layerGroup([AppioClaudio1, AppioClaudio2, AppioClaudio3, AppioClaudio4, AppioClaudio5, AppioClaudio6, 
     AppioClaudio7, AppioClaudio8, AppioClaudio9]);
-AppioClaudio.addTo(map);
+
 
 //Altro
-var Altro1 = L.marker([41.83209734707517, 12.599034238975383]).bindPopup("Perlage").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Altro2 = L.marker([41.848558587990304, 12.597752381304176]).bindPopup("Mister TAMO").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Altro3 = L.marker([41.82683548708564, 12.583777194794914]).bindPopup("Ma SI! Bar Tavola Calda").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Altro4 = L.marker([41.8563439415532, 12.673846663751162]).bindPopup("Dom").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Altro5 = L.marker([41.86032204476431, 12.688402081304806]).bindPopup("Due Punto Zero").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Altro6 = L.marker([41.836556593502614, 12.633258883155861]).bindPopup("Buddha Smile").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Altro7 = L.marker([41.814773137880465, 12.604046275958696]).bindPopup("N°1").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-
+var Altro1 = L.marker([41.83209734707517, 12.599034238975383]).bindPopup("Perlage");
+var Altro2 = L.marker([41.848558587990304, 12.597752381304176]).bindPopup("Mister TAMO");
+var Altro3 = L.marker([41.82683548708564, 12.583777194794914]).bindPopup("Ma SI! Bar Tavola Calda");
+var Altro4 = L.marker([41.8563439415532, 12.673846663751162]).bindPopup("Dom");
+var Altro5 = L.marker([41.86032204476431, 12.688402081304806]).bindPopup("Due Punto Zero");
+var Altro6 = L.marker([41.836556593502614, 12.633258883155861]).bindPopup("Buddha Smile");
+var Altro7 = L.marker([41.814773137880465, 12.604046275958696]).bindPopup("N°1");
 var Altro = L.layerGroup([Altro1, Altro2, Altro3, Altro4, Altro5, Altro6, Altro7]);
-Altro.addTo(map);
+
 
 //Appio Latino
-var AppioLatino1 = L.marker([41.876454270641176, 12.518999027338126]).bindPopup("Enoteca Bonomi").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioLatino2 = L.marker([41.8763976965445, 12.515734810141673]).bindPopup("QIX drink bar").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioLatino3 = L.marker([41.87615006569213, 12.520208352469577]).bindPopup("Vinum Est Roma").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioLatino4 = L.marker([41.87314018349225, 12.520773725485634]).bindPopup("Hopificio").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioLatino5 = L.marker([41.880251620718056, 12.511646352469825]).bindPopup("Rebacco").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var AppioLatino6 = L.marker([41.87839875941948, 12.516271510141783]).bindPopup("Ristorante Buono Enoteca e Cocktail Bar").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-
+var AppioLatino1 = L.marker([41.876454270641176, 12.518999027338126]).bindPopup("Enoteca Bonomi");
+var AppioLatino2 = L.marker([41.8763976965445, 12.515734810141673]).bindPopup("QIX drink bar");
+var AppioLatino3 = L.marker([41.87615006569213, 12.520208352469577]).bindPopup("Vinum Est Roma");
+var AppioLatino4 = L.marker([41.87314018349225, 12.520773725485634]).bindPopup("Hopificio");
+var AppioLatino5 = L.marker([41.880251620718056, 12.511646352469825]).bindPopup("Rebacco");
+var AppioLatino6 = L.marker([41.87839875941948, 12.516271510141783]).bindPopup("Ristorante Buono Enoteca e Cocktail Bar");
 var AppioLatino = L.layerGroup([AppioLatino1, AppioLatino2, AppioLatino3, AppioLatino4, AppioLatino5, AppioLatino6]);
-AppioLatino.addTo(map);
 
 
+var markers = L.markerClusterGroup();
+markers.addLayer(Tuscolano);
+markers.addLayer(DonBosco);
+markers.addLayer(AppioClaudio);
+markers.addLayer(Altro);
+markers.addLayer(AppioLatino);
+map.addLayer(markers);
+
+markers.on('click', function(e) {
+  map.panTo(e.latlng);
+});
 
--- Municipi/Municipio XI/script.js
+++ Municipi/Municipio XI/script.js
@@ -10,19 +10,24 @@ var customIcon = L.icon({
   L.Marker.prototype.options.icon = customIcon;
 
 //Portuense
-var Portuense1 = L.marker([41.87194133399641, 12.47061163522821]).bindPopup("Städlin").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Portuense2 = L.marker([41.869529791163934, 12.466729481305283]).bindPopup("Stazione 38").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Portuense3 = L.marker([41.86641229971461, 12.465168696649256]).bindPopup("Enoteca Uva Luppolo E Bistrot").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Portuense4 = L.marker([41.86522434047658, 12.468634237124832]).bindPopup("I Siciliani").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Portuense5 = L.marker([41.86259211855145, 12.47080029664913]).bindPopup("SHAVI BAR").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Portuense6 = L.marker([41.84950797036802, 12.44641785487317]).bindPopup("Bistrot 139").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Portuense7 = L.marker([41.85602062875366, 12.446986338976695]).bindPopup("Rossibar").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Portuense8 = L.marker([41.85844049634241, 12.454816356173207]).bindPopup("Bistrot Alleria - La Caffetteria dal 1980").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Portuense9 = L.marker([41.86604129857583, 12.471722181305095]).bindPopup("Bottega Arconti").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-var Portuense10 = L.marker([41.86877622421897, 12.47230668315759]).bindPopup("Bar India").on('click', function(e) {map.setView(e.target.getLatLng(), 16);});
-
+var Portuense1 = L.marker([41.87194133399641, 12.47061163522821]).bindPopup("Städlin");
+var Portuense2 = L.marker([41.869529791163934, 12.466729481305283]).bindPopup("Stazione 38");
+var Portuense3 = L.marker([41.86641229971461, 12.465168696649256]).bindPopup("Enoteca Uva Luppolo E Bistrot");
+var Portuense4 = L.marker([41.86522434047658, 12.468634237124832]).bindPopup("I Siciliani");
+var Portuense5 = L.marker([41.86259211855145, 12.47080029664913]).bindPopup("SHAVI BAR");
+var Portuense6 = L.marker([41.84950797036802, 12.44641785487317]).bindPopup("Bistrot 139");
+var Portuense7 = L.marker([41.85602062875366, 12.446986338976695]).bindPopup("Rossibar");
+var Portuense8 = L.marker([41.85844049634241, 12.454816356173207]).bindPopup("Bistrot Alleria - La Caffetteria dal 1980");
+var Portuense9 = L.marker([41.86604129857583, 12.471722181305095]).bindPopup("Bottega Arconti");
+var Portuense10 = L.marker([41.86877622421897, 12.47230668315759]).bindPopup("Bar India");
 var Portuense = L.layerGroup([Portuense1, Portuense2, Portuense3, Portuense4, Portuense5, Portuense6, Portuense7, Portuense8, 
   Portuense9, Portuense10]);
-Portuense.addTo(map);
 
 
+var markers = L.markerClusterGroup();
+markers.addLayer(Portuense);
+map.addLayer(markers);
+
+markers.on('click', function(e) {
+  map.panTo(e.latlng);
+});
